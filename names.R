3cb748b1f480a4918364ccda6a2f81fdea4ced8b
Jordan Kassof <jkassof@rockco.com>
2017-09-20 20:30:37 -0400

    Add Jordans name print

---
+++ names.R
@@ -0,0 +1,17 @@
+# Names.R
+# Created by Breakout 2
+# Date: 9/20/17
+# Tell us what your name is!
+
+# Original Repo-Owner, please write in here
+print("Hello, my name is Jordan Kassof")
+
+# Collaborators: Repeat this above command with your name.
+# Don't do this *for* other people.  Just let them pull the repo, add a line for their own name, commit, and push it!
+# Remember not to do this all at once.  Have one group member do it at a time to prevent conflicts.
+
+print("")
+
+print("")
+
+print("")
